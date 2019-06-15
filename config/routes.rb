0c2a3a046c46df3f651bd5e7ab99c482267d6895
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-06-15 17:51:26 +0900

    new back up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   get 'top' => 'root#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'books#index'
+
+  post'books' => 'books#create'
   
+  resources :books, only: [:new, :create, :index, :show]
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_102006) do
+ActiveRecord::Schema.define(version: 2019_06_15_035747) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "Title"
+    t.text "Opinion"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.text "image_id"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
