c22abbc32ada28b239640d83bf7ad72d17b29798
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-06-17 13:03:02 +0900

    nenotame

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a57dec6f4a6c29e9101903ced1df66a519ba591d2fe9d252122f0b2763fec82aee8ce0dc951b4d7bfcbfd544d0f59a662fbb359cd03606760a0079416325d11d'
